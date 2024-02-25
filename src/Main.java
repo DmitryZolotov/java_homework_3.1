8945e7d7d0f447679f1859138fdd1ca52deba727
Dmitry Zolotov <zolotov_dmitry@mail.ru>
2024-02-25 15:30:15 +0300

    add Project

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int ticketPrice = 15999;
+        int milePrice = 20;
+        int mileAmount = ticketPrice / milePrice;
+        System.out.println(mileAmount);
+    }
+}
